2228d1a4000e65f9c30fb3631e6977c4566aedbf
blackbrokkoli <kastomat@gmail.com>
2023-04-25 13:22:23 +0200

    import capitals

--- 4_full/main.py
+++ 4_full/main.py
@@ -6,6 +6,25 @@ from kivy.modules import inspector
 from kivy.config import Config
 # import BoxLayout
 from kivy.uix.boxlayout import BoxLayout
+import pandas as pd
+from models.card import Card
+
+
+def import_cards_from_csv(file_path):
+    cards_df = pd.read_csv(file_path)
+    cards = cards_df.to_dict(orient='records')
+
+    for card_data in cards:
+        card = Card.filter(front=card_data['front']).first()
+        if not card:
+            new_card = Card(front=card_data['front'], back=card_data['back'], type="learn")
+            new_card.save()
+
+    # print number of cards in db
+    print(f"We now have {Card.count()} cards in the database.")
+
+# import_cards_from_csv("world_capitals.csv")
+
 
 class MainApp(App):
     def build(self):
