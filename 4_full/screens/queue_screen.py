ba58d12540428b95c713dfab6958a0ef7f3e8edc
blackbrokkoli <kastomat@gmail.com>
2023-04-25 13:33:52 +0200

    implement basic non-functional queue UI

--- 4_full/screens/queue_screen.py
+++ 4_full/screens/queue_screen.py
@@ -1,4 +1,33 @@
 from kivy.uix.screenmanager import Screen
+from kivy.properties import StringProperty, ObjectProperty, BooleanProperty
+import random
+from models.card import Card
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
 
 class QueueScreen(Screen):
-    pass
+    flashcard = ObjectProperty(None)
+
+    def load_next_card(self):
+        # get a random card from the database
+        card = random.choice(Card.select())
+        self.flashcard = card  
+
+
+
+    # def on_enter(self, *args):
+    #     self.load_next_card()      
+
+    def on_enter(self):
+        self.load_next_card()
+        flashcard_widget = FlashCard(front=self.flashcard.front, back=self.flashcard.back)
+        self.add_widget(flashcard_widget)
+
+class FlashCard(BoxLayout):
+    front = StringProperty("")
+    back = StringProperty("")
+    revealed = BooleanProperty(False)
+
+    def reveal_card (self):
+        self.flashcard.revealed = True
